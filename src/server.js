8ed486922acae891eae99f524d93d36d85feaf6f
rainerio19 <rai.inerio@hotmail.com>
2024-06-24 17:47:20 -0300

    feat(config): node server run

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-import node from node
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  // Aqui você lida com as requisições HTTP
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
+});
+
+const PORT = 3000; // Escolha uma porta que esteja disponível
+
+server.listen(PORT, () => {
+    console.log(`Servidor está rodando em http://localhost:${PORT}`);
+});
